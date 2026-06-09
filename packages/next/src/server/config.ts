21eab0b84f008c32b7827848c770e2f14969d3ae
Tim Neutkens <tim@timneutkens.nl>
2026-06-09 17:17:50 +0200

    Remove render server adapter

--- packages/next/src/server/config.ts
+++ packages/next/src/server/config.ts
@@ -1826,15 +1826,13 @@ export default async function loadConfig(
   }
 
   // Original implementation continues below...
-  if (!process.env.__NEXT_PRIVATE_RENDER_WORKER) {
-    try {
-      loadWebpackHook()
-    } catch (err) {
-      // this can fail in standalone mode as the files
-      // aren't traced/included
-      if (!process.env.__NEXT_PRIVATE_STANDALONE_CONFIG) {
-        throw err
-      }
+  try {
+    loadWebpackHook()
+  } catch (err) {
+    // this can fail in standalone mode as the files
+    // aren't traced/included
+    if (!process.env.__NEXT_PRIVATE_STANDALONE_CONFIG) {
+      throw err
     }
   }
 
